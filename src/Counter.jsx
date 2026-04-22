2c74ffe1d9679944f10f9d7516e2a163730c758d
mikaila <mikailamohammad23@gmail.com>
2026-04-22 17:06:23 +0100

    initial commit

---
+++ src/Counter.jsx
@@ -0,0 +1,27 @@
+
+function Counter(){
+
+    return(
+
+        <div className="counter">
+        <div className="heading">
+        <h1>Tasbah</h1>
+        <button>Dark/Light</button>
+        </div>
+
+        <div className="main-continer">
+        <div className="number-display">
+        0000000000
+        </div>
+        
+        <div className="buttons">
+        <button className="add-btn">Add</button>
+        <button  className="minus-btn">Minus</button>
+        <button className="reset-btn">Reset</button>
+        </div>
+        
+        </div>
+        </div>
+    )
+}
+export default Counter
